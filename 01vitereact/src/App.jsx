8547619c837ab9c45ed9b3cbb7b3d405f275d33a
SahirAli7 <saiyedsahirali122@gmail.com>
2024-07-14 16:53:40 +0530

    Here I learnt how to make react element,evaluatedExpression. Also made custom react element(basically replaces jsx).

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,11 +2,12 @@
 import Chai from "./chai"
 
 function App() {
-  
+const username = 'SahirAli'
 
   return (
     <>
     <Chai />
+    <h1>{username}</h1>
     <h1>Chai is ready</h1>
     <p>Test para</p>
     </>
