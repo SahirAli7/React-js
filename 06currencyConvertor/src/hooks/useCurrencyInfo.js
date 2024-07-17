edb131175572a8e4a0777f4a2b24ab51cf5e893b
SahirAli7 <saiyedsahirali122@gmail.com>
2024-07-18 00:46:19 +0530

    Made a project of real-time currency converter , learned how to call API's , basic debugging,new hooks and more.

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -3,7 +3,7 @@ import {useState , useEffect} from 'react'
 function useCurrencyInfo(currency){
     const [data,setData] = useState({})
     useEffect(() => {
-        fetch(`https://v6.exchangerate-api.com/v6/3b18a2bc3d7b87454a105965/latest/${currency}`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
         console.log(data);
